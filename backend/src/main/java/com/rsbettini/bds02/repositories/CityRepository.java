001e335feba40ef41499951ba05cedd64115587c
rsbettini <robson.bettini@hotmail.com>
2022-01-18 20:11:10 -0300

    Implementing tests

---
+++ backend/src/main/java/com/rsbettini/bds02/repositories/CityRepository.java
@@ -0,0 +1,10 @@
+package com.rsbettini.bds02.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rsbettini.bds02.entities.City;
+
+public interface CityRepository extends JpaRepository<City, Long>{
+
+}
+
